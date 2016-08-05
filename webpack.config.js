602390d1c7a4426d048791630e4953f03f7ceed7
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-08-04 19:21:18 -0700

    Adds esri-leaflet as a npm dependency. leaflet is recognized as an external library L

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  entry: "./js/script.js",
+  entry: './js/script.js',
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js'
+  },
+  externals: {
+    'leaflet': 'L'
   },
   node: {
     fs: 'empty' // this is needed for Handlebars to work
